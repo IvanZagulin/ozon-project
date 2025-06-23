f1236cf29dee60b9218b006d7aa720aa465fc5d3
Иван Загулин <88832557+IvanZagulin@users.noreply.github.com>
2025-06-23 19:24:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -28,4 +28,5 @@ def index():
     return render_template("index.html", logs=LOG_STORE)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True, threaded=True)
+
