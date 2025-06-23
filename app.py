4d95fa807d7fd8784f0a0aa65ed3b3e1341b4b3f
Иван Загулин <88832557+IvanZagulin@users.noreply.github.com>
2025-06-23 11:27:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ def maintenance():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Render задаёт свой PORT
-    app.run(host='0.0.0.0', port=port
+    app.run(host='0.0.0.0', port=port)
